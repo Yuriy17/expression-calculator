388a4e8b55c3b91f06cda5d4237c29d33f9d5f6f
Yuriy17 <yuriy_sh@zoho.com>
2019-10-09 05:23:50 +0300

    expression-calculator/feat: realize line loop

--- src/index.js
+++ src/index.js
@@ -4,7 +4,74 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let prior = {
+            '+': 1,
+            '-': 1,
+            '*': 2,
+            '/': 2
+        },
+        brackets = {
+            open: '(',
+            close: ')'
+        },
+        numbers = [],
+        number = '',
+        operators = [],
+        line_array = [],
+        line = expr.replace(/\s/g, '');
+    for (let i; i < line.length; i++) {
+        switch (line[i]) {
+            case '+':
+                operators.push('+');
+                if (number.length > 0) {
+                    numbers.push(parseInt(number));
+                    number = '';
+                }
+                break;
+
+            case '-':
+                operators.push('-');
+                if (number.length > 0) {
+                    numbers.push(parseInt(number));
+                    number = '';
+                }
+                break;
+            case '*':
+                operators.push('*');
+                if (number.length > 0) {
+                    numbers.push(parseInt(number));
+                    number = '';
+                }
+                break;
+            case '/':
+                operators.push('/');
+                if (number.length > 0) {
+                    numbers.push(parseInt(number));
+                    number = '';
+                }
+                break;
+            case '(':
+                operators.push('(');
+                if (number.length > 0) {
+                    numbers.push(parseInt(number));
+                    number = '';
+                }
+                break;
+            case ')':
+                operators.push(')');
+                if (number.length > 0) {
+                    numbers.push(parseInt(number));
+                    number = '';
+                }
+                break;
+            default:
+                number += line[i];
+                if(i==line.length-1){
+                    numbers.push(parseInt(number));
+                }
+                break;
+        }
+    }
 }
 
 module.exports = {
